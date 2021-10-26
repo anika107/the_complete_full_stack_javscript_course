9a845d1d423dca6c7443d0c50adcc09b3a6e82da
anika107 <atahsin171107@bscse.uiu.ac.bd>
2021-10-27 02:59:00 +0600

    Section 2 Content no. 7 Understanding component props and re-rendering

--- src/Application.js
+++ src/Application.js
@@ -1,22 +1,48 @@
-import {Component} from 'react';
+import React, {Component} from 'react';
+import HighScore from './HighScore';
+
 class Application extends Component {
     // eslint-disable-next-line no-useless-constructor
     constructor(props){
         super(props);
+
+        this.state = {
+            count: 0,
+            overTen: false
+        }
     }
     
-    componentWillMount(props, state){
-
+    handleClick = () => {
+        this.setState({count: this.state.count+1})
     }
-    componentDidMount(props, state){
-       console.log("Mounted with ", props, state) 
+
+    componentDidUpdate(props, state){
+        if(this.state.count > 10 && this.state.count !== state.count){
+            console.log("Updating over ten")
+            this.setState({overTen: true})
+        }
+    }
+
+    resetCount = (e) => {
+        console.log("the event is", e)
+        this.setState({
+            count: 0,
+            overTen: false
+        })
     }
     render(){
-        let name = "Nick";
+        let {count} = this.state;
         return(
         <div>
-           <h1>Hello {name}</h1>
-           <span>This</span>
+           <h1>You clicked the button {count} times</h1>
+                <HighScore
+                    overTen={this.state.overTen}
+                    onReset={this.resetCount}
+               />
+           <span>
+                <button onClick={(e) => this.handleClick()}>Click Me</button>
+                 
+           </span>
         </div>
         
        );
